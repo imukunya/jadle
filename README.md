54fe8d287b16bc900f759d4cbbcbb26ede85726a
imukunya <64461160+imukunya@users.noreply.github.com>
2021-02-09 08:49:53 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Site-Maintenance App
+
+## Brief Description
+
+An application to practice week four
+
+
+## Authors
+
+* [Ian Mukunya](https://github.com/imukunya)
+
+## Set Up Instructions
+
+To start using this project:
+
+* `git clone https://github.com/imukunya/jadle.git`
+* `cd jadle`
+* `gradle build`
+
+To set up PostgreSql:
+````
+files found in resources/db
+````
+
+## Technologies Used
+
+* Java
+* JUnit
+* Gradle
+* PostgreSQL
+* GIT
+* Maven
+
+## Contact Information
+
+* [Ian Mukunya](mailto:ianmukunya@gmail.com?subject=[GitHub]%20Private%20and%20Confidential)
---
+++ src/main/java/models/Restaurant.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Restaurant {
+    private String name;
+    private String address;
+    private String zipcode;
+    private String phone;
+    private String website;
+    private String email;
+    private int id;
+
+    public Restaurant(String name, String address, String zipcode, String phone) {
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.phone = phone;
+        this.website = "no website listed";
+        this.email = "no email available";
+    }
+
+    public Restaurant(String name, String address, String zipcode, String phone, String website, String email) {
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.phone = phone;
+        this.website = website;
+        this.email = email;
+    }
+
+}
